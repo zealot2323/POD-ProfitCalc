741252957da7edec59ac727acf83f44ae33c71ce
Eric Pratt <zealot2323@gmail.com>
2025-08-15 20:08:43 +0200

    Groundwork for histric orders

--- src/main.js
+++ src/main.js
@@ -7,26 +7,42 @@ const printifyData = await router("printify");
 console.log(printifyData);
 console.log(printifyData[0]);
 console.log(printifyData[0].products);
-
 const container = document.querySelector("#app-content");
 
-printifyData[0].products.forEach(product => {
-  let row = document.createElement("tr");
-  const rowData = [product.name, formatToDollars(product.etsyPrice), formatToDollars(product.cost), formatToDollars(product.profitPerItem), `${product.profitMargin.toFixed(0)}%`, product.sales, formatToDollars(product.totalProfit)];
-  
-  const productImg = document.createElement('td');
-  productImg.innerHTML = `<img src="${product.image}" class="table"/>`
-  row.appendChild(productImg);
+function getActive() {
+  printifyData[0].products.forEach(product => {
+    let row = document.createElement("tr");
+    const rowData = [product.name, formatToDollars(product.etsyPrice), formatToDollars(product.cost), formatToDollars(product.profitPerItem), `${product.profitMargin.toFixed(0)}%`, product.sales, formatToDollars(product.totalProfit)];
+    
+    const productImg = document.createElement('td');
+    productImg.innerHTML = `<img src="${product.image}" class="table"/>`
+    row.appendChild(productImg);
 
-  rowData.forEach(data => {
-    const cell = document.createElement('td');
-    cell.textContent = data;
-    row.appendChild(cell);
+    rowData.forEach(data => {
+      const cell = document.createElement('td');
+      cell.textContent = data;
+      row.appendChild(cell);
+    });
+
+    container.appendChild(row);
   });
 
-  container.appendChild(row);
+  const totalProfit = document.querySelector("#totalProfit");
+
+  totalProfit.textContent = `Total Profit for Active Items: ${formatToDollars(printifyData[0].totalProfit)}`;
+};
+
+ const active = document.querySelector("#active");
+ active.addEventListener("click", () => {
+  //show active products and profits
+  getActive();
 });
 
-const totalProfit = document.querySelector("#totalProfit");
+ const historic = document.querySelector("#historic");
+  historic.addEventListener("click", () => {
+  //show active products and profits
+  getActive();
+  console.log('foo');
+});
 
-totalProfit.textContent = `Total Profit for Active Items: ${formatToDollars(printifyData[0].totalProfit)}`;
+getActive();
